52bed9ceecf33289efb10efd5ebdf517573d3505
Amir Yunas <ayunas@gmail.com>
2021-10-11 19:06:36 -0400

    created two tests for matchHouses

--- js/__tests__/matchHouses.test.js
+++ js/__tests__/matchHouses.test.js
@@ -1,8 +1,16 @@
+const { expect } = require('@jest/globals');
 const countMatchHouses = require('../matchHouses');
 
 describe("matchHouses", () => {
 
-    test("it exists",() => {
-        expect(countMatchHouses()).toBe(true);
+    test("1 house => 6", () => {
+        expect(countMatchHouses(1)).toBe(6);
+    });
+
+    test("2 houses => 11", () => {
+        expect(countMatchHouses(2)).toBe(11);
+
+
     })
+
 })
--- js/matchHouses.js
+++ js/matchHouses.js
@@ -1,6 +1,8 @@
+// calculate the number of matches required to create h number of houses.  One house requires 6 matches (4 walls and 2 for the roof). Each house is adjacent to the other house and shares one side.
 
-function countMatchHouses() {
-    return true;
+
+function countMatchHouses(houses) {
+    return 6;
 }
 
 module.exports = countMatchHouses;
