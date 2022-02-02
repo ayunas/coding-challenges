90ac1825e077b6fcfc3149cac510a27cbfa148b6
Amir Yunas <ayunas@gmail.com>
2022-02-02 16:56:37 -0500

    mergeArrays

--- js/matchHouses.js
+++ js/matchHouses.js
@@ -1,6 +1,5 @@
 // calculate the number of matches required to create h number of houses.  One house requires 6 matches (4 walls and 2 for the roof). Each house is adjacent to the other house and shares one side.
 
-
 function countMatchHouses(houses) {
     return 6;
 }
